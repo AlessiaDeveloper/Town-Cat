7af77614e3e7b9e53f96c04cc8dfc7ff0025d20d
Alessia Mori <49532458+GattonaSviluppatrice@users.noreply.github.com>
2024-07-11 11:11:53 +0200

    rimesso apposto font

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
-import * as Animatable from "react-native-animatable";
+import React, { useState, useEffect } from "react";
 import * as Font from "expo-font";
 import LoadingScreen from "./.expo/components/LoadingScreen";
+import * as NavigationBar from "expo-navigation-bar";
 
 import Home from "./.expo/components/Home";
 
@@ -14,47 +13,14 @@ async function loadFonts() {
 
 loadFonts();
 
-// import fonts from "./assets/fonts";
-
-// Separate FactoryButton Component
-const FactoryButton = ({ onPress, label }) => (
-  <TouchableOpacity style={styles.factoryButton} onPress={onPress}>
-    <Text>{label}</Text>
-  </TouchableOpacity>
-);
-
 export default function App() {
-  const [score, setScore] = useState(0);
-  const [factories, setFactories] = useState(0);
-  const [showNormalImage, setShowNormalImage] = useState(true);
-
-  // Stato per gestire la visibilità delle immagini
-
-  // Update score every second based on the number of factories
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (factories > 0) {
-        setScore((prevScore) => prevScore + factories);
-      }
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [factories]);
-
-  const handlePress = useCallback(() => {
-    setScore((prevScore) => prevScore + 1);
-    setShowNormalImage(false); // Nascondi l'immagine normale
-    // Trigger the animation on press
-    if (buttonRef.current) {
-      buttonRef.current.bounceIn().then(() => {
-        // Ripristina l'immagine allo stato iniziale dopo l'animazione
-        setShowNormalImage(true);
-      });
+    async function hideNavigationBar() {
+      await NavigationBar.setVisibilityAsync("hidden");
+      await NavigationBar.setBehaviorAsync("overlay-swipe");
     }
+    hideNavigationBar();
   }, []);
-
-  // Ref to access the button component
-  const buttonRef = useRef(null);
 
   const [isLoading, setIsLoading] = useState(true);
 
@@ -65,64 +31,3 @@ export default function App() {
   }, []);
   return isLoading ? <LoadingScreen /> : <Home></Home>;
 }
-
-const styles = StyleSheet.create({
-  arancio: {
-    padding: 10,
-    flexDirection: "row",
-    justifyContent: "space-around",
-    width: "100%",
-    height: "20px",
-    backgroundColor: "#FF6666",
-  },
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#5D2E8C",
-  },
-  score: {
-    fontSize: 24,
-    margin: 20,
-    fontWeight: "bold",
-  },
-  button: {
-    padding: 10,
-  },
-  edifici: {
-    flexDirection: "row",
-    alignSelf: "stretch",
-    backgroundColor: "#CCFF66",
-    borderBlockEndColor: "#FF6666",
-    borderBottomWidth: 2,
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  imageEdifici: {
-    width: 50,
-    height: 50,
-    resizeMode: "contain",
-    margin: 10,
-  },
-  image: {
-    width: 300,
-    height: 300,
-    resizeMode: "contain",
-  },
-  factories: {
-    fontFamily: "LuckiestGuy-Regular",
-    color: "#2EC4B6",
-    backgroundColor: "#CCFF66",
-    borderRadius: 5,
-    padding: 10,
-    textShadowColor: "#5D2E8C",
-    fontSize: 24,
-    margin: 20,
-  },
-  factoryButton: {
-    padding: 10,
-    backgroundColor: "#5D2E8C",
-    margin: 10,
-    borderRadius: 5,
-  },
-});
